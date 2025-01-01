6e78baf5f6e453f2c575c9d12e7ea39b5e42cfc9
sasineelakandan <devasasi887@gmail.com>
2025-01-01 15:42:33 +0530

    finish22

--- docresrva/next.config.ts
+++ docresrva/next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['documentsdrs3.s3.eu-north-1.amazonaws.com'],
   },
+  eslint: {
+    ignoreDuringBuilds: true, // This disables ESLint during builds
+  }
 };
 
 export default nextConfig;
