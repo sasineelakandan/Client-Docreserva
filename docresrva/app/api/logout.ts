7d20a3dfb0528f43151a484f9a0dcc6695c55cdd
sasineelakandan <devasasi887@gmail.com>
2025-01-02 16:03:31 +0530

    finish

--- docresrva/app/api/logout.ts
+++ docresrva/app/api/logout.ts
@@ -4,9 +4,10 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method not allowed' });
   }
-
-  // Clear the access token cookie
-  res.setHeader('Set-Cookie', 'accessToken=; Max-Age=0; Path=/; HttpOnly; Secure; SameSite=Strict; Domain=.docreserva.site');
+  res.setHeader('Set-Cookie', [
+    'accessToken=; Max-Age=0; Path=/; HttpOnly; Secure; SameSite=Strict; Domain=.docreserva.site',
+    'refreshToken=; Max-Age=0; Path=/; HttpOnly; Secure; SameSite=Strict; Domain=.docreserva.site',
+  ]);
 
   return res.status(200).json({ message: 'Logout successful' });
 }
