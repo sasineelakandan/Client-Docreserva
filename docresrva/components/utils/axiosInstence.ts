3cda7274e493fb257612935ff7c9ceb26be20404
sasineelakandan <devasasi887@gmail.com>
2025-02-02 16:57:35 +0530

    correct instence

--- docresrva/components/utils/axiosInstence.ts
+++ docresrva/components/utils/axiosInstence.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Create an Axios instance
 const axiosInstance = axios.create({
-//    baseURL: 'http://localhost:8001', // Replace with your API base URL
+
     baseURL: 'https://api.docreserva.site',
 });
 
