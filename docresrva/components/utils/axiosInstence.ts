7c00b7e4404274aac2fe9c8b0485d141321875ed
sasineelakandan <devasasi887@gmail.com>
2025-02-02 14:48:02 +0530

    axios interseptor use

---
+++ docresrva/components/utils/axiosInstence.ts
@@ -0,0 +1,29 @@
+// axiosInstance.js
+import axios from 'axios';
+
+// Create an Axios instance
+const axiosInstance = axios.create({
+   baseURL: 'http://localhost:8001', // Replace with your API base URL
+    // baseURL: 'https://api.mrfit.life',
+});
+
+// Add a request interceptor
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const accesstoken = localStorage.getItem('accesstoken'); // Adjust the key as necessary
+        const refreshtoken = localStorage.getItem('refreshtoken'); // Adjust the key as necessary
+        if (refreshtoken && accesstoken) {
+            config.headers['accesstoken'] = `Bearer ${accesstoken}`;
+            config.headers['refreshtoken'] = `Bearer ${refreshtoken}`;
+        }
+        console.log(config.headers,"config.headers")
+        return config; // Return the modified config
+    },
+    (error) => {
+        // Handle the error
+        return Promise.reject(error);
+    }
+);
+
+
+export default axiosInstance;
