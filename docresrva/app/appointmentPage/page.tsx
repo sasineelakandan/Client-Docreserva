bb1e7689ce0a0a43d33b3cf0d0384840e925b119
sasineelakandan <devasasi887@gmail.com>
2025-02-03 07:23:59 +0530

    clear al bugs

--- docresrva/app/appointmentPage/page.tsx
+++ docresrva/app/appointmentPage/page.tsx
@@ -82,12 +82,7 @@ const AppointmentsList: React.FC = () => {
   }, [appointments, filterDate, filterPatient]);
   
 
-  const formatTime = (time24: string): string => {
-    const [hours, minutes] = time24.split(":").map(Number);
-    const period = hours >= 12 ? "PM" : "AM";
-    const hours12 = hours % 12 || 12;
-    return `${hours12}:${minutes.toString().padStart(2, '0')} ${period}`;
-  };
+ 
 
   const onSubmit: SubmitHandler<FormValues> = async (data) => {
     const payload = {
